a559ddd714c9704bfc5fa29309497a1b3b6517ba
arr19422 <arr19422@uvg.edu.gt>
2020-03-26 22:50:39 -0600

    Laboratorio Completo

---
+++ Assets/Scripts/Escala.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escala : MonoBehaviour
+{
+    public float velocidad = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.localScale += Vector3.one * velocidad * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.X))
+        {
+            transform.localScale -= Vector3.one * velocidad * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Oscilaciones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilaciones : MonoBehaviour
+{
+
+    public float amplitud = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(Mathf.Sin(Time.time), 1) * amplitud;
+    }
+}
